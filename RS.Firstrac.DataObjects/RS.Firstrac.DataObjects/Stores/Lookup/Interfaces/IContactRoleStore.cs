f562043689c0189bc71a42b0e5e8b8128b9061f4
Michael Quinn <mquinn@edgecoholdings.com>
2025-05-27 19:11:11 +0000

    Added contact role methods ***NO_CI*** Imported from Changeset 19486

---
+++ RS.Firstrac.DataObjects/RS.Firstrac.DataObjects/Stores/Lookup/Interfaces/IContactRoleStore.cs
@@ -0,0 +1,56 @@
+﻿// ***********************************************************************
+// Assembly         : RS.Firstrac.DataObjects
+// Author           : Michael Quinn
+// Created          : 5/27/2025 12:52:05 PM
+//
+// Last Modified By : Michael Quinn
+// Last Modified On : 5/27/2025 12:52:05 PM
+// ***********************************************************************
+// <copyright file="IContactRoleStore.cs" company="EdgeCo Holdings, Inc.">
+//     Copyright (c) 2025 EdgeCo Holdings, Inc. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using RS.Common.Data.API6.Interfaces.Generic;
+using RS.Firstrac.BusinessObjects.Models.Lookup.Interfaces;
+using RS.Firstrac.DataObjects.Stores.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS.Firstrac.DataObjects.Stores.Lookup.Interfaces
+{
+	public interface IContactRoleStore : IStoreBase<IContactRole>
+	{
+		#region Methods
+
+		/// <summary>
+		/// Deletes the specified identifier.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns>Task&lt;IAPIOperationResult&lt;System.Boolean&gt;&gt;.</returns>
+		Task<IAPIOperationResult<bool>> Delete(int id, string deletedBy);
+		/// <summary>
+		/// Gets the specified identifier.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns>Task&lt;IAPIOperationResult&lt;TInterface&gt;&gt;.</returns>
+		Task<IAPIOperationResult<IContactRole>> Get(int id);
+		/// <summary>
+		/// Gets all.
+		/// </summary>
+		/// <returns>Task&lt;IAPIOperationResult&lt;IEnumerable&lt;TInterface&gt;&gt;&gt;.</returns>
+		Task<IAPIOperationResult<IEnumerable<IContactRole>>> GetAll(bool? activeOnly, Dictionary<string, object>? filterBy = null, bool? exactMatch = true, bool? mutuallyExclusive = false, bool? includeNavigationProperties = true);
+
+		/// <summary>
+		/// Saves the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns>Task&lt;IAPIOperationResult&lt;System.Boolean&gt;&gt;.</returns>
+		Task<IAPIOperationResult<bool>> Save(IContactRole model);
+
+		#endregion
+	}
+}
