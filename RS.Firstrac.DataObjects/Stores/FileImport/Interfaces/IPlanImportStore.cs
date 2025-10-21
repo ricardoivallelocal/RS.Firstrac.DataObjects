d15a1d0c05971df5c2b2df24e0d5c93462a84c25
ricardo fraire <ricardoivalle@hotmail.com>
2025-10-21 11:28:14 -0400

    updates

---
+++ RS.Firstrac.DataObjects/Stores/FileImport/Interfaces/IPlanImportStore.cs
@@ -0,0 +1,39 @@
+﻿// ***********************************************************************
+// Assembly         : RS.Firstrac.DataObjects
+// Author           : ricardo.fraire
+// Created          : 04-21-2023
+//
+// Last Modified By : ricardo.fraire
+// Last Modified On : 04-24-2023
+// ***********************************************************************
+// <copyright file="IAssetGroupCusipLinkStore.cs" company="RS.Firstrac.DataObjects">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using RS.Firstrac.BusinessObjects.Models.Admin.Interfaces;
+using RS.Firstrac.DataObjects.Stores.Interfaces;
+using RS.Common.Data.API6.Interfaces.Generic;
+using RS.Firstrac.BusinessObjects.Models.FileImport.Interfaces;
+
+namespace RS.Firstrac.DataObjects.Stores.Admin.Interfaces
+{
+    /// <summary>
+    /// Interface IAssetGroupCusipLinkStore
+    /// Extends the <see cref="RS.Firstrac.DataObjects.Stores.Interfaces.IStoreBase{RS.Firstrac.BusinessObjects.Models.Admin.Interfaces.IAssetGroupCusipLink}" />
+    /// </summary>
+    /// <seealso cref="RS.Firstrac.DataObjects.Stores.Interfaces.IStoreBase{RS.Firstrac.BusinessObjects.Models.Admin.Interfaces.IAssetGroupCusipLink}" />
+    public interface IPlanImportStore : IStoreBase<IPlanImportRequest>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Saves the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns>Task&lt;IAPIOperationResult&lt;System.Boolean&gt;&gt;.</returns>
+        Task<IAPIOperationResult<bool>> Save(IPlanImportRequest model);
+
+        #endregion
+    }
+}
